efa71de5112a8c0c0424212a45ad09d059867e2c
Gurunathan <gurunathan.ece.sec@gmail.com>
2025-05-13 07:25:42 +0530

    Updated committed

--- Pincode Parser/src/main/java/com/guru/Parser/SQLInsertGeneratorFromJSON.java
+++ Pincode Parser/src/main/java/com/guru/Parser/SQLInsertGeneratorFromJSON.java
@@ -6,18 +6,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.*;
 import java.util.*;
 
-public class SQLInsertGeneratorFromJSON
-{
-    public static void begin()
-    {
-        System.out.println("Beginning");
+public class SQLInsertGeneratorFromJSON {
+    public static void begin() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode root = null;
-        try {
-            root = objectMapper.readTree(new File("pinCode.json"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        JsonNode root = objectMapper.readTree(new File("pinCode.json"));
         JsonNode records = root.get("records");
 
         Set<String> states = new LinkedHashSet<>();
@@ -37,54 +29,46 @@ public class SQLInsertGeneratorFromJSON
         String district;
         String districtKey;
 
-
-        for(JsonNode record : records)
-        {
+        for (JsonNode record : records) {
             state = record.get("statename").asText().trim();
             district = record.get("district").asText().trim();
             pinCode = record.get("pincode").asInt();
             districtKey = district + "|" + state;
 
-            if(!stateMap.containsKey(state))
-            {
+            if (!stateMap.containsKey(state)) {
                 stateMap.put(state, stateId++);
 
                 states.add("INSERT INTO state VALUES (" + (stateId - 1) + ", '" + state + "');");
             }
 
-            if(!districtMap.containsKey(districtKey))
-            {
+            if (!districtMap.containsKey(districtKey)) {
                 districtMap.put(districtKey, districtId++);
 
-                districts.add("INSERT INTO district VALUES (" + (districtId - 1) + ", '" + district + "', " + stateMap.get(state) + ");");
+                districts.add("INSERT INTO district VALUES (" + (districtId - 1) + ", '" + district + "', "
+                        + stateMap.get(state) + ");");
             }
 
-            if(!pinCodeSet.contains(pinCode))
-            {
-                pinCodes.add("INSERT INTO pinCode VALUES (" + pinCodeId++ + ", " + pinCode + ", " + districtMap.get(districtKey) + ");");
+            if (!pinCodeSet.contains(pinCode)) {
+                pinCodes.add("INSERT INTO pinCode VALUES (" + pinCodeId++ + ", " + pinCode + ", "
+                        + districtMap.get(districtKey) + ");");
 
                 pinCodeSet.add(pinCode);
             }
         }
 
-        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("data.sql")))
-        {
-            for(String line : states)
-                bufferedWriter.write(line + "\n");
-            bufferedWriter.flush();
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("data.sql"));
 
-            for(String line : districts)
-                bufferedWriter.write(line + "\n");
-            bufferedWriter.flush();
+        for (String line : states)
+            bufferedWriter.write(line + "\n");
+        bufferedWriter.flush();
 
-            for(String line : pinCodes)
-                bufferedWriter.write(line + "\n");
-            bufferedWriter.flush();
-        } catch (RuntimeException | IOException e) {
-            throw new RuntimeException(e);
-        }
+        for (String line : districts)
+            bufferedWriter.write(line + "\n");
+        bufferedWriter.flush();
 
-        System.out.println("Ended");
+        for (String line : pinCodes)
+            bufferedWriter.write(line + "\n");
+        bufferedWriter.flush();
 
     }
 }
